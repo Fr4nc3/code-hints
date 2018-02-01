2ce5f838f077fa42f1f19dc0b78846526aac0dbd
Fr4nc3 <ff2214@columbia.edu>
2018-01-31 22:12:23 -0500

    python datastructures

---
+++ python/algorithms/node_linked_list_test.py
@@ -0,0 +1,26 @@
+from node import UnorderedList
+
+mylist = UnorderedList()
+mylist.add(31)
+mylist.add(77)
+mylist.add(17)
+mylist.add(93)
+mylist.add(26)
+mylist.add(54)
+
+print(mylist.size())
+print(mylist.search(93))
+print(mylist.search(100))
+
+mylist.add(100)
+print(mylist.search(100))
+print(mylist.size())
+
+mylist.remove(54)
+print(mylist.size())
+mylist.remove(93)
+print(mylist.size())
+mylist.remove(31)
+print(mylist.size())
+print(mylist.search(93))
+
