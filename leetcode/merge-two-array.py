d74e3183e13ea66efa9542791b4d1f185315eb85
Francia Riesco <francekakumei@gmail.com>
2022-07-19 11:45:22 -0400

    Update merge-two-array.py

--- leetcode/merge-two-array.py
+++ leetcode/merge-two-array.py
@@ -1,8 +1,8 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def mergeTwoLists(self, l1, l2):
         # maintain an unchanging reference to node ahead of the return node.
